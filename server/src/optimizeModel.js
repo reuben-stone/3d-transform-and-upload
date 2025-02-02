08482eda5c8aa91ff596723018eb6fae56d14e28
Reuben Stone <reuben.stone@diageo.com>
2025-02-02 14:07:30 +0000

    added random color adjustment to optimisation process

--- server/src/optimizeModel.js
+++ server/src/optimizeModel.js
@@ -16,20 +16,53 @@ async function processOBJ(objBuffer) {
         const tempFilePath = path.join(__dirname, 'temp.obj');
         fs.writeFileSync(tempFilePath, objBuffer);
 
-        // Convert .obj to GLTF from the temporary file
+        // Convert .obj to GLTF 
         console.log('Converting OBJ to GLTF...');
-        const gltf = await obj2gltf(tempFilePath);
-        const gltfBuffer = Buffer.from(JSON.stringify(gltf));
-
-        console.log('Optimizing GLTF with gltf-pipeline...');
-        const optimized = await gltfPipeline.processGltf(gltfBuffer, {
-            dracoOptions: { compressionLevel: 10 },
-        });
-
-        console.log('OPTIMIZED', optimized);
+        let gltf = await obj2gltf(tempFilePath);
 
         // Clean up the temporary file
         fs.unlinkSync(tempFilePath);
+
+        // Add random colors to the GLTF materials
+        if (gltf && gltf.nodes) {
+            console.log("Applying random colors...");
+
+            const randomColorPalette = [
+                [1, 0, 0], // Red
+                [0, 1, 0], // Green
+                [0, 0, 1], // Blue
+                [1, 1, 0], // Yellow
+                [1, 0, 1], // Magenta
+                [0, 1, 1], // Cyan
+            ];
+
+            gltf.nodes.forEach((node) => {
+                if (node.mesh !== undefined && gltf.meshes[node.mesh]) {
+                    gltf.meshes[node.mesh].primitives.forEach((primitive) => {
+                        if (!gltf.materials) gltf.materials = [];
+
+                        const randomColor = randomColorPalette[Math.floor(Math.random() * randomColorPalette.length)];
+                        const materialIndex = gltf.materials.length;
+                        gltf.materials.push({
+                                pbrMetallicRoughness: {
+                                baseColorFactor: [...randomColor, 1], // RGB + Alpha
+                                metallicFactor: 0.1, // Slightly metallic
+                                roughnessFactor: 0.9, // High roughness
+                            },
+                        });
+
+                        primitive.material = materialIndex;
+                    });
+                }
+            });
+        }
+
+        console.log("Optimizing GLTF with gltf-pipeline...");
+        const gltfBuffer = Buffer.from(JSON.stringify(gltf));
+
+        const optimized = await gltfPipeline.processGltf(gltfBuffer, {
+            dracoOptions: { compressionLevel: 10 },
+        });
 
         // Return the optimized GLB
         console.log('Optimization complete');
